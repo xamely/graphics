1879b8da0184d8b6817bb22e4a92deda29587842
xamely <m.xeniya.a@ya.ru>
2019-09-22 15:01:09 +0300

    First version

---
+++ Filling (lab 2)/PictureFilling/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PictureFilling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(22, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(596, 382);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(649, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(667, 188);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Filling";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Filling (lab 2)/PictureFilling/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureFilling
+{
+    public partial class Form1 : Form
+    {
+        Color CurrentColor = Color.Black; //Default color
+        Point CurrentPoint; //Current Position
+        Point PrevPoint; //Previous Position
+        bool isPressed;
+        Graphics g;
+        private Bitmap map;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            map = new Bitmap(panel1.Width, panel1.Height, g);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult D = colorDialog1.ShowDialog();
+            if (D == System.Windows.Forms.DialogResult.OK)
+                CurrentColor = colorDialog1.Color;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            CurrentPoint = e.Location;
+            if (checkBox1.Checked) fill_area();
+            else isPressed = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                PrevPoint = CurrentPoint;
+                CurrentPoint = e.Location;
+                paint_simple();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private void paint_simple()
+        {
+            Pen p = new Pen(CurrentColor);
+            g.DrawLine(p, PrevPoint, CurrentPoint);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void fill_area()
+        {
+            Pen p = new Pen(CurrentColor);
+            map = new Bitmap(panel1.Width, panel1.Height);
+            tmp_fill(p, CurrentPoint);
+        }
+
+        private void tmp_fill(Pen p, Point TempCurrentPoint)
+        {
+            Point left_point = TempCurrentPoint;
+            while (left_point.X > 0 && p.Color != map.GetPixel(left_point.X - 1, left_point.Y))
+                left_point.X -= 1;
+            g.DrawLine(p, TempCurrentPoint, left_point);
+            Point right_point = TempCurrentPoint;
+            while (right_point.X < (map.Width - 1) && p.Color != map.GetPixel(right_point.X + 1, right_point.Y))
+                right_point.X += 1;
+            g.DrawLine(p, right_point, TempCurrentPoint);
+            for (int i = left_point.X; i <= right_point.X; i++)
+            {
+                if (left_point.Y < (map.Height - 1) && p.Color != map.GetPixel(i, left_point.Y + 1))
+                    tmp_fill(p, new Point(i, left_point.Y + 1));
+                if (left_point.Y > 0 && p.Color != map.GetPixel(i, left_point.Y - 1))
+                    tmp_fill(p, new Point(i, left_point.Y - 1));
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
