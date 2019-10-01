4a1c22c02075406c5d493826dca13eab75326140
alekgor <alekgor@sfedu.ru>
2019-10-01 21:54:22 +0300

    Сделал для трех первых задач

---
+++ L-Systems(lab 5)/L-Systems(lab 5)/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace L_Systems_lab_5_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.area = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.area)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // area
+            // 
+            this.area.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.area.Location = new System.Drawing.Point(1, 2);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(638, 446);
+            this.area.TabIndex = 0;
+            this.area.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(675, 159);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(90, 39);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(675, 399);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(90, 39);
+            this.Refresh.TabIndex = 2;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ".",
+            "01. Кривая Коха",
+            "02. Снежинка Коха",
+            "03. Остров Коха"});
+            this.comboBox1.Location = new System.Drawing.Point(645, 116);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(645, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox2.Location = new System.Drawing.Point(751, 82);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(32, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(642, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Глубина фрактала:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.area);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.area)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox area;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ L-Systems(lab 5)/L-Systems(lab 5)/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace L_Systems_lab_5_
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 2;
+
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.Clear(Color.White);
+            
+        }
+
+
+        public void drawF(int x, int y, int len, double angle, PaintEventArgs e, int it)
+        {
+            Graphics g = e.Graphics;
+            double x1, y1;
+            len = (int)(len * zoom);
+            x1 = x + len * Math.Sin(angle * Math.PI * 2 / 360.0);
+            y1 = y + len * Math.Cos(angle * Math.PI * 2 / 360.0);
+            g.DrawLine(new Pen(Color.Black, len / 40), x, area.Height - y, (int)x1, area.Height - (int)y1);
+
+            if (it < 8)
+            {
+                Random rnd = new Random();//+ rnd.Next(-c, -c)
+
+                drawF((int)x1, (int)y1, (int)(len * 0.8), angle - 60, e, it + 1);
+                drawF((int)x1, (int)y1, (int)(len * 0.8), angle - 30, e, it + 1);
+                drawF((int)x1, (int)y1, (int)(len * 0.8), angle + 30, e, it + 1);
+                drawF((int)x1, (int)y1, (int)(len * 0.8), angle + 60, e, it + 1);
+
+
+            }
+        }
+
+
+        double zoom = 0.5;
+
+        public PointF F(float x, float y, float len, float angle, int it)
+        {
+            
+            float x1, y1;
+            len = (float)(len * zoom);
+            x1 = x + len * (float)Math.Sin(angle * Math.PI * 2 / 360.0);
+            y1 = y + len * (float)Math.Cos(angle * Math.PI * 2 / 360.0);
+
+            g.DrawLine(new Pen(Color.Black), x, (float)(area.Height - y), x1, (float)(area.Height - (y1)));
+            area.Invalidate();
+            //area.Update();
+            return new PointF(x1, y1);
+        }
+
+
+
+
+        //Start - начальная строка
+        //Rule - правило
+        //it - кол-во итераций
+        public String convertRule(String starts, String rule, int it)
+        {
+            String s = "";
+            for (int i = 0; i < it; i++)
+            {
+                for (int j = 0; j < starts.Length; j++)
+                {
+                    if (starts[j] == 'F')
+                    {
+                        s += rule;
+                    }
+                    else if (starts[j] == '+' || starts[j] == '-')
+                    {
+                        s += starts[j];
+                    }
+                }
+                starts = s;
+                s = "";
+            }
+            return starts;
+        }
+        public void drawFract(float x, float y, float len, float napr, float angle, String s)
+        {
+            int c = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'F')
+                {
+                    c++;
+                }
+            }
+            PointF P = new PointF(x, y);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'F')
+                {
+                    P = F(P.X, P.Y, len, napr, 0);
+                    area.Update();
+                }
+                else if (s[i] == '+')
+                {
+                    napr += angle;
+
+                }
+                else if (s[i] == '-')
+                {
+                    napr -= angle;
+                }
+            }
+        }
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.Clear(Color.White);
+            comboBox1.SelectedIndex = 0;
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != 0)
+            {
+                int X, Y, it;
+                X = 150;
+                Y = 150;
+                it = 3;
+                it = Convert.ToInt32(comboBox2.Items[comboBox2.SelectedIndex]);
+
+                string[] text = new string[10];
+                float len=0;
+                float azimut=0;
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    string file_name = @"..\..\test_fractals\1.Кривая Коха.txt";
+
+                    text = File.ReadAllLines(file_name);
+                    label1.Text = text[1];
+                    len = (float)(200.0 / Math.Pow(Math.Log(200, 6), it - 1));
+                    azimut = 30;
+
+                }
+                if (comboBox1.SelectedIndex == 2)
+                {
+                    string file_name = @"..\..\test_fractals\2.Снежинка Коха.txt";
+
+                    text = File.ReadAllLines(file_name);
+                    label1.Text = text[1];
+                    len = (float)(200.0 / Math.Pow(Math.Log(200, 6), it - 1));
+                    azimut = 30;
+
+                }
+
+                if (comboBox1.SelectedIndex == 3)
+                {
+                    string file_name = @"..\..\test_fractals\3.Остров Коха.txt";
+
+                    text = File.ReadAllLines(file_name);
+                    label1.Text = text[1];
+                    len = (float)(200.0 / Math.Pow(Math.Log(200, 6), it ));
+                    azimut = 0;
+                    Y -= 50;
+                }
+
+
+                int i = 0;
+                int angle = 0;
+                string start = "";
+                string rule = "";
+                while (text[i] != ".")
+                {
+                    string str = text[i];
+                    if (i == 0)
+                    {
+                        start = str.Split()[0];
+                        angle = Convert.ToInt32(str.Split()[1]);
+                    }
+                    else
+                    {
+                        rule = str.Split()[2];
+                    }
+                    i++;
+                }
+               // float len = (float)(200.0 / Math.Pow(Math.Log(200, 6), it - 1));
+                string s = convertRule(start, rule, it);
+                drawFract(X, Y, len, azimut, angle, s);
+            }
+            area.Invalidate();
+        }
+    }
+}
