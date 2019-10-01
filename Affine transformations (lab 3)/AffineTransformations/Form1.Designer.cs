85d330b3aaf03ab9808144005351c557d82feffd
xamely <m.xeniya.a@ya.ru>
2019-10-01 16:53:19 +0300

    Add figures and rotation

---
+++ Affine transformations (lab 3)/AffineTransformations/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace AffineTransformations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.area = new System.Windows.Forms.PictureBox();
+            this.refresh = new System.Windows.Forms.Button();
+            this.dotButton = new System.Windows.Forms.RadioButton();
+            this.lineButton = new System.Windows.Forms.RadioButton();
+            this.polygonButton = new System.Windows.Forms.RadioButton();
+            this.lineOptionsBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.polygonOptionsBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.area)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // area
+            // 
+            this.area.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.area.Location = new System.Drawing.Point(28, 32);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(601, 475);
+            this.area.TabIndex = 0;
+            this.area.TabStop = false;
+            this.area.MouseDown += new System.Windows.Forms.MouseEventHandler(this.area_MouseDown);
+            this.area.MouseMove += new System.Windows.Forms.MouseEventHandler(this.area_MouseMove);
+            this.area.MouseUp += new System.Windows.Forms.MouseEventHandler(this.area_MouseUp);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(681, 97);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(144, 53);
+            this.refresh.TabIndex = 1;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // dotButton
+            // 
+            this.dotButton.AutoSize = true;
+            this.dotButton.Location = new System.Drawing.Point(688, 180);
+            this.dotButton.Name = "dotButton";
+            this.dotButton.Size = new System.Drawing.Size(51, 21);
+            this.dotButton.TabIndex = 2;
+            this.dotButton.TabStop = true;
+            this.dotButton.Text = "Dot";
+            this.dotButton.UseVisualStyleBackColor = true;
+            // 
+            // lineButton
+            // 
+            this.lineButton.AutoSize = true;
+            this.lineButton.Location = new System.Drawing.Point(688, 207);
+            this.lineButton.Name = "lineButton";
+            this.lineButton.Size = new System.Drawing.Size(56, 21);
+            this.lineButton.TabIndex = 3;
+            this.lineButton.TabStop = true;
+            this.lineButton.Text = "Line";
+            this.lineButton.UseVisualStyleBackColor = true;
+            // 
+            // polygonButton
+            // 
+            this.polygonButton.AutoSize = true;
+            this.polygonButton.Location = new System.Drawing.Point(688, 234);
+            this.polygonButton.Name = "polygonButton";
+            this.polygonButton.Size = new System.Drawing.Size(80, 21);
+            this.polygonButton.TabIndex = 4;
+            this.polygonButton.TabStop = true;
+            this.polygonButton.Text = "Polygon";
+            this.polygonButton.UseVisualStyleBackColor = true;
+            // 
+            // lineOptionsBox
+            // 
+            this.lineOptionsBox.FormattingEnabled = true;
+            this.lineOptionsBox.Items.AddRange(new object[] {
+            "Move",
+            "Rotate 90 dgr around center",
+            "Search for the intersection point of two lines"});
+            this.lineOptionsBox.Location = new System.Drawing.Point(681, 302);
+            this.lineOptionsBox.Name = "lineOptionsBox";
+            this.lineOptionsBox.Size = new System.Drawing.Size(223, 24);
+            this.lineOptionsBox.TabIndex = 5;
+            this.lineOptionsBox.SelectedIndexChanged += new System.EventHandler(this.lineOptionsBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(685, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Choose line option:";
+            // 
+            // polygonOptionsBox
+            // 
+            this.polygonOptionsBox.FormattingEnabled = true;
+            this.polygonOptionsBox.Items.AddRange(new object[] {
+            "Move",
+            "Rotate around center",
+            "Rotate around point",
+            "Center scaling ",
+            "Point scaling"});
+            this.polygonOptionsBox.Location = new System.Drawing.Point(681, 397);
+            this.polygonOptionsBox.Name = "polygonOptionsBox";
+            this.polygonOptionsBox.Size = new System.Drawing.Size(223, 24);
+            this.polygonOptionsBox.TabIndex = 7;
+            this.polygonOptionsBox.SelectedIndexChanged += new System.EventHandler(this.PolygonOptionsBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(685, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Choose polygon option:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 545);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.polygonOptionsBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lineOptionsBox);
+            this.Controls.Add(this.polygonButton);
+            this.Controls.Add(this.lineButton);
+            this.Controls.Add(this.dotButton);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.area);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.area)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox area;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.RadioButton dotButton;
+        private System.Windows.Forms.RadioButton lineButton;
+        private System.Windows.Forms.RadioButton polygonButton;
+        private System.Windows.Forms.ComboBox lineOptionsBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox polygonOptionsBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Affine transformations (lab 3)/AffineTransformations/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AffineTransformations
+{
+    public partial class Form1 : Form
+    {
+        Point CurrentPoint; //Current Position
+        Point LastPoint; 
+        bool isPressed;
+        Graphics g;
+        List<Point> polyPoints;
+        bool first = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.Clear(Color.White);
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.KeyPreview = true;
+            polyPoints = new List<Point>();
+            //points = new List<Point>();
+
+            lineButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            dotButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            polygonButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+        }
+
+        private void radioButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            polyPoints.Clear();
+        }
+
+        private void area_MouseDown(object sender, MouseEventArgs e)
+        {
+            CurrentPoint = e.Location;
+        }
+
+        private void area_MouseMove(object sender, MouseEventArgs e)
+        {
+            //    if (lineButton.Checked)
+            //    {
+            //        LastPoint = e.Location;
+            //        points.Add(CurrentPoint);
+            //    }
+        }    
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.S)
+            {
+                g.DrawLine(new Pen(Color.Black), polyPoints[0], polyPoints[polyPoints.Count - 1]);
+                first = true;
+                area.Invalidate();
+            }
+        }
+
+        private void area_MouseUp(object sender, MouseEventArgs e)
+        {
+            Pen p = new Pen(Color.Black);
+            if (lineButton.Checked)
+            {
+                LastPoint = e.Location;
+                if (lineOptionsBox.SelectedIndex == 2 && polyPoints.Count > 0)
+                {
+                    g.DrawLine(p, CurrentPoint, LastPoint);
+
+                    double a1 = (CurrentPoint.Y - LastPoint.Y) / (CurrentPoint.X - LastPoint.X);
+                    double a2 = (polyPoints[0].Y - polyPoints[1].Y) / (polyPoints[0].X - polyPoints[1].X);
+
+                    double b1 = CurrentPoint.Y - a1 * CurrentPoint.X;
+                    double b2 = polyPoints[0].Y - a2 * polyPoints[0].X;
+
+                    int x = (int)((b2 - b1) / (a1 - a2));
+                    
+
+                    if (!((x < Math.Max(CurrentPoint.X, polyPoints[0].X)) || (x > Math.Min(LastPoint.X, polyPoints[1].X))))
+                    {
+                        x = (int)(-((CurrentPoint.X * LastPoint.Y - LastPoint.X * CurrentPoint.Y) * (polyPoints[1].X - polyPoints[0].X) - (polyPoints[0].X * polyPoints[1].Y - polyPoints[1].X * polyPoints[0].Y) * (LastPoint.X - CurrentPoint.X)) / ((CurrentPoint.Y - LastPoint.Y) * (polyPoints[1].X - polyPoints[0].X) - (polyPoints[0].Y - polyPoints[1].Y) * (LastPoint.X - CurrentPoint.X)));
+                        int y = ((polyPoints[0].Y - polyPoints[1].Y) * (-x) - (polyPoints[0].X * polyPoints[1].Y - polyPoints[1].X * polyPoints[0].Y)) / (polyPoints[1].X - polyPoints[0].X);
+                        g.FillEllipse(new SolidBrush(Color.Red), new Rectangle(x, y, 3, 3));
+                       
+                    }
+                    area.Invalidate();
+                }
+                else
+                {
+                    g.DrawLine(p, CurrentPoint, LastPoint);
+                    polyPoints.Add(CurrentPoint);
+                    polyPoints.Add(LastPoint);
+                    area.Invalidate();
+                }
+            }
+            else if (dotButton.Checked)
+            {
+                LastPoint = e.Location;
+                g.FillEllipse(new SolidBrush(Color.Black), new Rectangle(LastPoint.X, LastPoint.Y, 3, 3));
+                area.Invalidate();
+            }
+            else if (polygonButton.Checked)
+            {
+                if (first)
+                {
+                    polyPoints.Clear();
+                    LastPoint = e.Location;
+                    polyPoints.Add(LastPoint);
+                    first = false;
+                }
+                else
+                {
+                    g.DrawLine(p, LastPoint, CurrentPoint);
+                    LastPoint = CurrentPoint;
+                    polyPoints.Add(CurrentPoint);
+                    area.Invalidate();
+                }
+            }
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.FillRectangle(new SolidBrush(Color.White), 0, 0, area.Width, area.Height);
+            polyPoints.Clear();
+            first = true;
+        }
+
+
+        private Point rotate(Point center, int angle, Point old_point)
+        {
+            double[,] move_mat1 = { { 1, 0, 0 }, { 0, 1, 0 }, { -center.X, -center.Y, 1 } };
+            double[,] move_mat2 = { { 1, 0, 0 }, { 0, 1, 0 }, { center.X, center.Y, 1 } };
+            double cos = Math.Cos(Math.PI / 180 * angle);
+            double sin = Math.Sin(Math.PI / 180 * angle);
+            double[,] rotation_mat = { { cos, sin, 0 }, { -sin, cos, 0 }, { 0, 0, 1 } };
+
+            Point new_point = new Point(0, 0);
+
+            multiplication(old_point, move_mat1, ref new_point);
+            multiplication(new_point, rotation_mat, ref new_point);
+            multiplication(new_point, move_mat2, ref new_point);
+
+            return new_point;
+        }
+        private void multiplication(Point point, double[,] mat, ref Point new_point)
+        {
+            double x = point.X;
+            double y = point.Y;
+            double[,] vec = { { 0, 0, 0 }, { x, y, 1 }, { 0, 0, 0 } };
+            double[,] res = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    for (int k = 0; k < 3; k++)
+                        res[i, j] += vec[i, k] * mat[k, j];
+            new_point.X = (int)res[1, 0];
+            new_point.Y = (int)res[1, 1];
+        }
+
+        private Point move(Point old_point, int x, int y)
+        {
+            double[,] mat = { { 1, 0, 0 }, { 0, 1, 0 }, { x, y, 1 } };
+            Point new_point = new Point(0, 0);
+            multiplication(old_point, mat, ref new_point);
+            return new_point;
+        }
+
+        private void lineOptionsBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lineButton.Checked && polyPoints.Count > 0)
+            {
+                if (lineOptionsBox.SelectedIndex == 0)
+                {
+                    var dialogX = new InputBox("Введите смещение по X (целое число)");
+                    var dialogY = new InputBox("Введите смещение по Y (целое число)");
+                    int dx = 0, dy = 0;
+                    if (dialogX.ShowDialog() == DialogResult.Cancel) return;
+                    if (!int.TryParse(dialogX.ResultText, out dx)) return;
+                    if (dialogY.ShowDialog() == DialogResult.Cancel) return;
+                    if (!int.TryParse(dialogY.ResultText, out dy)) return;
+                    
+                    g.DrawLine(new Pen(Color.White), polyPoints[0], polyPoints[1]);
+                    polyPoints[0] = move(polyPoints[0], dx, dy);
+                    polyPoints[1] = move(polyPoints[1], dx, dy);
+                    g.DrawLine(new Pen(Color.Black), polyPoints[0], polyPoints[1]);
+                    area.Invalidate();
+                }
+                else if (lineOptionsBox.SelectedIndex == 1)
+                {
+                    Point center = new Point((polyPoints[0].X + polyPoints[1].X) / 2, (polyPoints[0].Y + polyPoints[1].Y) / 2);
+                    g.DrawLine(new Pen(Color.White), polyPoints[0], polyPoints[1]);
+                    polyPoints[0] = rotate(center, 90, polyPoints[0]);
+                    polyPoints[1] = rotate(center, 90, polyPoints[1]);
+                    g.DrawLine(new Pen(Color.Black), polyPoints[0], polyPoints[1]);
+                    area.Invalidate();
+                }
+                else if (lineOptionsBox.SelectedIndex == 2)
+                {
+
+                }
+            }
+        }
+
+
+        private void PolygonOptionsBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (polygonButton.Checked && polyPoints.Count > 0)
+            {
+                if (polygonOptionsBox.SelectedIndex == 0)
+                {
+                    var dialogX = new InputBox("Введите смещение по X (целое число)");
+                    var dialogY = new InputBox("Введите смещение по Y (целое число)");
+                    int dx = 0, dy = 0;
+                    if (dialogX.ShowDialog() == DialogResult.Cancel) return;
+                    if (!int.TryParse(dialogX.ResultText, out dx)) return;
+                    if (dialogY.ShowDialog() == DialogResult.Cancel) return;
+                    if (!int.TryParse(dialogY.ResultText, out dy)) return;
+
+                    g.DrawLine(new Pen(Color.White), polyPoints[0], polyPoints[polyPoints.Count - 1]);
+                    for (int i=0; i<polyPoints.Count-1;i++)
+                        g.DrawLine(new Pen(Color.White), polyPoints[i], polyPoints[i + 1]);
+
+                    for (int i = 0; i < polyPoints.Count; i++)
+                    {
+                        if (i != polyPoints.Count - 1)
+                        {
+                            polyPoints[i] = move(polyPoints[i], dx, dy);
+                            g.DrawLine(new Pen(Color.Black), polyPoints[i], move(polyPoints[i + 1], dx, dy));
+                        }
+                        else
+                        {
+                            polyPoints[i] = move(polyPoints[i], dx, dy);
+                            g.DrawLine(new Pen(Color.Black), polyPoints[i], polyPoints[0]);
+                        }
+                    }
+                    area.Invalidate();
+                }
+
+            }
+        }    
+    }
+}
