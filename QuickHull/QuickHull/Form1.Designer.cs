97cdbdc9ce820e65c1673969093ca28436764dd1
xamely <m.xeniya.a@ya.ru>
2019-10-05 12:45:20 +0300

    Algorithm works

---
+++ QuickHull/QuickHull/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace QuickHull
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.area = new System.Windows.Forms.PictureBox();
+            this.builder = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.area)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // area
+            // 
+            this.area.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.area.Location = new System.Drawing.Point(31, 35);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(603, 440);
+            this.area.TabIndex = 0;
+            this.area.TabStop = false;
+            this.area.MouseClick += new System.Windows.Forms.MouseEventHandler(this.area_MouseClick);
+            // 
+            // builder
+            // 
+            this.builder.Enabled = false;
+            this.builder.Location = new System.Drawing.Point(662, 393);
+            this.builder.Name = "builder";
+            this.builder.Size = new System.Drawing.Size(131, 46);
+            this.builder.TabIndex = 1;
+            this.builder.Text = "Build the span";
+            this.builder.UseVisualStyleBackColor = true;
+            this.builder.Click += new System.EventHandler(this.builder_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(662, 323);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(131, 45);
+            this.refresh.TabIndex = 2;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 502);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.builder);
+            this.Controls.Add(this.area);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.area)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox area;
+        private System.Windows.Forms.Button builder;
+        private System.Windows.Forms.Button refresh;
+    }
+}
+
---
+++ QuickHull/QuickHull/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickHull
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        List<Point> points;
+
+        public Form1()
+        {
+            InitializeComponent();
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.Clear(Color.White);
+            points = new List<Point>();
+        }
+
+        private void area_MouseClick(object sender, MouseEventArgs e)
+        {
+            g.FillEllipse(new SolidBrush(Color.Black), new Rectangle(e.X - 2, e.Y - 2, 4, 4));
+            points.Add(new Point(e.X, e.Y));
+            if (points.Count() > 2)
+            {
+                builder.Enabled = true;
+            }
+            area.Invalidate();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.FillRectangle(new SolidBrush(Color.White), 0, 0, area.Width, area.Height);
+            points.Clear();
+        }
+
+        private void builder_Click(object sender, EventArgs e)
+        {
+            // finding left and right points
+            Point min_point = points[0];
+            Point max_point = points[0];
+            for (int i = 1; i < points.Count; i++)
+            {
+                if (points[i].X < min_point.X)
+                    min_point = points[i];
+                if (points[i].X > max_point.X)
+                    max_point = points[i];
+            }
+
+            quickHull(min_point, max_point, points_relatively_to_line(min_point, max_point, points, 1), 1);
+            quickHull(min_point, max_point, points_relatively_to_line(min_point, max_point, points, -1), -1);
+        }
+
+        List<Point> points_relatively_to_line(Point left, Point right, List<Point> points, int side)
+        {
+            List<Point> result = new List<Point>();
+            for (int i = 0; i < points.Count; i++)
+                if (findSide(left, right, points[i]) == side)
+                    result.Add(points[i]);
+
+           return result;
+        }
+
+        int lineDist(Point p1, Point p2, Point p)
+        {
+            return Math.Abs((p.X - p1.X) * (p2.Y - p1.Y) - (p.Y - p1.Y) * (p2.X - p1.X));
+        }
+
+        int findSide(Point p1, Point p2, Point p)
+        {
+            int val = (p.X - p1.X) * (p2.Y - p1.Y) - (p.Y - p1.Y) * (p2.X - p1.X);
+
+            if (val > 0)
+                return 1;
+            if (val < 0)
+                return -1;
+            return 0;
+        }
+
+        // 1 = right than line between points 
+        // -1 = left than line between points 
+        private void quickHull(Point left, Point right, List<Point> points, int side)
+        {
+            if (points.Count == 0)
+            {
+                g.DrawLine(new Pen(Color.Black), left, right);
+                area.Invalidate();
+                return;
+            }
+
+            // finding point with max distance
+            int max_dist = 0;
+            Point max_point = new Point();
+            for (int i = 0; i < points.Count; i++)
+            {
+                int temp = lineDist(left, right, points[i]);
+                if (temp > max_dist)
+                {
+                    max_point = points[i];
+                    max_dist = temp;
+                }
+            }
+            
+            // points to the left of the line (upper or lower)
+            quickHull(left, max_point, points_relatively_to_line(left, max_point, points, side), side);
+
+            // points to the right of the line (upper or lower)
+            quickHull(right, max_point, points_relatively_to_line(right, max_point, points, -side), -side);
+        }
+    }
+}
