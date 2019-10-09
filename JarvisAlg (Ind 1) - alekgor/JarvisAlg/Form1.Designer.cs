9a439ae55b744254374e96f3f35cddea8ee0d275
alekgor <alekgor@sfedu.ru>
2019-10-09 17:51:52 +0300

    done

---
+++ JarvisAlg (Ind 1) - alekgor/JarvisAlg/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace JarvisAlg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.area = new System.Windows.Forms.PictureBox();
+            this.refresh = new System.Windows.Forms.Button();
+            this.dotButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.area)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // area
+            // 
+            this.area.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.area.Location = new System.Drawing.Point(21, 26);
+            this.area.Margin = new System.Windows.Forms.Padding(2);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(451, 386);
+            this.area.TabIndex = 1;
+            this.area.TabStop = false;
+            this.area.MouseDown += new System.Windows.Forms.MouseEventHandler(this.area_MouseDown);
+            this.area.MouseUp += new System.Windows.Forms.MouseEventHandler(this.area_MouseUp);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(526, 59);
+            this.refresh.Margin = new System.Windows.Forms.Padding(2);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(108, 43);
+            this.refresh.TabIndex = 2;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // dotButton
+            // 
+            this.dotButton.AutoSize = true;
+            this.dotButton.Location = new System.Drawing.Point(558, 142);
+            this.dotButton.Margin = new System.Windows.Forms.Padding(2);
+            this.dotButton.Name = "dotButton";
+            this.dotButton.Size = new System.Drawing.Size(42, 17);
+            this.dotButton.TabIndex = 3;
+            this.dotButton.TabStop = true;
+            this.dotButton.Text = "Dot";
+            this.dotButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 443);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dotButton);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.area);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.area)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox area;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.RadioButton dotButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JarvisAlg (Ind 1) - alekgor/JarvisAlg/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JarvisAlg
+{
+    public partial class Form1 : Form
+    {
+        Point CurrentPoint; //Current Position
+        Point LastPoint;
+        Graphics g;
+        List<Point> polyPoints;
+        bool first = true;
+        bool can_draw = true;
+        public Form1()
+        {
+            InitializeComponent();
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.Clear(Color.White);
+            //this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.KeyPreview = true;
+            polyPoints = new List<Point>();
+
+            dotButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+
+        }
+
+
+        private void radioButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.dotButton.Checked)
+            {
+                can_draw = true;
+                polyPoints.Clear();
+            }
+        }
+
+        private void area_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.CurrentPoint = e.Location;
+        }
+
+        private void area_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (this.dotButton.Checked)
+            {
+                LastPoint = e.Location;
+                polyPoints.Add(LastPoint);
+                g.FillEllipse(new SolidBrush(Color.Black), new Rectangle(LastPoint.X-1, LastPoint.Y-1, 4,4));
+                area.Invalidate();
+            }
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            polyPoints.Clear();
+            g.FillRectangle(new SolidBrush(Color.White), 0, 0, area.Width, area.Height);
+            this.dotButton.Checked = false;
+
+        }
+
+
+
+        private void Start_Button_Click(object sender, EventArgs e)
+        {
+            int n = this.polyPoints.Count;
+            List<Point> A = this.polyPoints;
+
+
+            List<Point> newA = new List<Point>();
+
+            if (this.polyPoints.Count > 2)
+            {
+                int m = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (A[i].X < A[m].X)
+                    {
+                        m = i;
+                    }
+                }
+
+
+                for (int i = m; i < n; i++)
+                {
+                    newA.Add(A[i]);
+                }
+                for (int i = 0; i < m; i++)
+                {
+                    newA.Add(A[i]);
+                }
+                this.label1.Text = newA[0].ToString();
+            }
+
+            List<bool> F = new List<bool>();
+            for (int i = 0; i < n; i++)
+                F.Add(false);
+
+            Point cur = newA[0];
+            Point start = newA[0];
+            List<Point> H = new List<Point>();
+            H.Add(cur);
+            int cid = 0;
+            while (F[0] == false)
+            {
+                int right = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (cid != i)
+                    {
+                        Tuple<int, int> t = this.RightCheckList(cur, newA[i], newA);
+                        if (t.Item1 == 1 && F[t.Item2]!=true)
+                        {
+                            cur = newA[t.Item2];
+                            cid = t.Item2;
+                            F[t.Item2] = true;
+                            if (cur != start)
+                                H.Add(cur);
+                        }
+                    }
+
+                }
+            }
+
+            cur = H[0];
+            for (int i = 1; i < H.Count; i++)
+            {
+
+                g.DrawLine(new Pen(Color.Black), cur, H[i]);
+                cur = H[i];
+                area.Invalidate();
+
+            }
+            g.DrawLine(new Pen(Color.Black),cur,H[0]);
+
+            area.Invalidate();
+        }
+
+        public Tuple<int, int> RightCheckList(Point O, Point A, List<Point> L)
+        {
+            int c = 0;
+            int cid = 0;
+            for (int i = 0; i < L.Count; i++)
+            {
+                if (RightCheck(O, A, L[i]))
+                {
+                    c++;
+                    cid = i;
+                }
+            }
+            Tuple<int, int> t = new Tuple<int, int>(c, cid);
+            return t;
+        }
+        /*
+        private void Start_Button_Click(object sender, EventArgs e)
+        {
+            int n = this.polyPoints.Count;
+            List<Point> A = this.polyPoints;
+            List<int> P = new List<int>();
+
+            for (int i = 0; i < n; i++)
+                P.Add(i);
+
+            if (this.polyPoints.Count > 2)
+            {
+
+                for (int i = 1; i < n; i++)
+                {
+                    if (A[P[i]].X < A[P[0]].X)
+                    {
+                        int x = P[i];
+                        P[i] = P[0];
+                        P[0] = x;
+                    }
+                }
+                this.label1.Text = A[P[0]].ToString() ;
+            }
+
+            List<int> H = new List<int>();
+            H.Add(P[0]);
+            P.RemoveAt(0);
+            P.Add(H[0]);
+
+
+            while (true ) {
+                int right = 0;
+                for (int i = 1; i < P.Count; i++) {
+                    if (  this.RightCheck(A[H[H.Count-1]],A[P[right]],A[P[i]])  ) {
+                        right = i;
+                    }
+                    if (P[right] == H[0])
+                        break;
+                    else {
+                        H.Add(P[right]);
+                        P.RemoveAt(right);
+                        this.label1.Text = H.Count.ToString();
+                    }
+                }
+            }
+
+        }*/
+
+
+
+        private bool RightCheck(Point O, Point A, Point B)
+        {
+            int ox = O.X;
+            int oy = O.Y;
+            int xa = A.X;
+            int ya = A.Y;
+            int xb = B.X;
+            int yb = B.Y;
+
+            int xam = xa - ox;
+            int yam = ya - oy;
+            int xbm = xb - ox;
+            int ybm = yb - oy;
+
+
+            if ((ybm * xam - xbm * yam) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
