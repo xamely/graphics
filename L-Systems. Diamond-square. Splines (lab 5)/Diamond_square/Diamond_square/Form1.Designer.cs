0f88a18e56ed49e2f6650999ef9820bcb143df71
xamely <m.xeniya.a@ya.ru>
2019-10-16 23:17:43 +0300

    Diamond-square

---
+++ L-Systems. Diamond-square. Splines (lab 5)/Diamond_square/Diamond_square/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Diamond_square
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.area = new System.Windows.Forms.PictureBox();
+            this.draw = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Coefficient = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.area)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // area
+            // 
+            this.area.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.area.Location = new System.Drawing.Point(16, 14);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(659, 481);
+            this.area.TabIndex = 0;
+            this.area.TabStop = false;
+            this.area.MouseDown += new System.Windows.Forms.MouseEventHandler(this.area_MouseDown);
+            this.area.MouseUp += new System.Windows.Forms.MouseEventHandler(this.area_MouseUp);
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(693, 68);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(178, 54);
+            this.draw.TabIndex = 1;
+            this.draw.Text = "Draw";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(693, 161);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(178, 54);
+            this.clear.TabIndex = 2;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(709, 334);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0,5";
+            // 
+            // Coefficient
+            // 
+            this.Coefficient.AutoSize = true;
+            this.Coefficient.Location = new System.Drawing.Point(712, 309);
+            this.Coefficient.Name = "Coefficient";
+            this.Coefficient.Size = new System.Drawing.Size(78, 17);
+            this.Coefficient.TabIndex = 4;
+            this.Coefficient.Text = "Coefficient:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 511);
+            this.Controls.Add(this.Coefficient);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.draw);
+            this.Controls.Add(this.area);
+            this.Name = "Form1";
+            this.Text = "Diamond";
+            ((System.ComponentModel.ISupportInitialize)(this.area)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox area;
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Coefficient;
+    }
+}
+
---
+++ L-Systems. Diamond-square. Splines (lab 5)/Diamond_square/Diamond_square/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diamond_square
+{
+    public partial class Form1 : Form
+    {
+        Point StartPoint;
+        Point LastPoint;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.Clear(Color.White);
+        }
+
+        private void area_MouseDown(object sender, MouseEventArgs e)
+        {
+            StartPoint = e.Location;
+        }
+
+        private void area_MouseUp(object sender, MouseEventArgs e)
+        {
+            LastPoint = e.Location;
+            Pen p = new Pen(Color.Black);
+            g.DrawLine(p, StartPoint, LastPoint);
+            area.Invalidate();
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+            double d;
+            double.TryParse(textBox1.Text, out d);
+            diamond_squareAsync(StartPoint, LastPoint, d);
+        }
+
+        private async void diamond_squareAsync(Point left, Point right, double r)
+        {
+            if (Math.Abs(left.X - right.X) == 1) return;
+
+            int length = (int)Math.Sqrt((left.X - right.X) * (left.X - right.X) + (left.Y - right.Y) * (left.Y - right.Y));
+
+            int new_h = (left.Y + right.Y) / 2 + new Random(length).Next((int)(-r * length), (int)(r * length));
+            Point h = new Point((left.X + right.X) / 2, new_h);
+
+            Pen p = new Pen(Color.White);
+            g.DrawLine(p, left, right);
+            p = new Pen(Color.Black);
+            g.DrawLine(p, left, h);
+            g.DrawLine(p, h, right);
+            
+            area.Invalidate();
+            await Task.Delay(100);
+            diamond_squareAsync(left, h, r);
+            diamond_squareAsync(h, right, r);
+            
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            area.Invalidate();
+        }
+    }
+}
