1b8d8fe636b5463845feb489a6ce76314469184d
n1gor <lazarev792@gmail.com>
2019-10-16 23:26:48 +0300

    3rd done

---
+++ L-Systems. Diamond-square. Splines (lab 5)/Bezie/Bezie/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Bezie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.area = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.area)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // area
+            // 
+            this.area.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.area.Location = new System.Drawing.Point(12, 12);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(413, 369);
+            this.area.TabIndex = 0;
+            this.area.TabStop = false;
+            this.area.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Area_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(447, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(449, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 78);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(449, 153);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Delete";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(449, 176);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Move";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(449, 130);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Draw";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 395);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.area);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.area)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox area;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ L-Systems. Diamond-square. Splines (lab 5)/Bezie/Bezie/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bezie
+{
+    public partial class Form1 : Form
+    {
+        Point CurrentPoint; //Current Position
+        Point LastPoint;
+        Graphics g;
+        List<Point> bPoints;
+        PointF[] result;
+        bool isDraw = false;
+        public Form1()
+        {
+            InitializeComponent();
+            area.Image = new Bitmap(area.Width, area.Height);
+            g = Graphics.FromImage(area.Image);
+            g.Clear(Color.White);
+            bPoints = new List<Point>();
+        }
+
+        int Fuctorial(int n) 
+        {
+            int res = 1;
+            for (int i = 1; i <= n; i++)
+                res *= i;
+            return res;
+        }
+        float polinom(int i, int n, float t)
+        {
+            return (Fuctorial(n) / (Fuctorial(i) * Fuctorial(n - i))) * (float)Math.Pow(t, i) * (float)Math.Pow(1 - t, n - i);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            area.Invalidate();
+            bPoints.Clear();
+        }
+
+        private void Area_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                CurrentPoint = e.Location;
+                bPoints.Add(CurrentPoint);
+                g.FillEllipse(new SolidBrush(Color.Black), new Rectangle(CurrentPoint.X, CurrentPoint.Y, 5, 5));
+                area.Invalidate();
+            }
+            else if (radioButton1.Checked)
+            {
+                CurrentPoint = e.Location;
+                for (int i = 0; i < bPoints.Count; i++)
+                    if (Math.Abs(CurrentPoint.X - bPoints[i].X) <= 5 && Math.Abs(CurrentPoint.Y - bPoints[i].Y) <= 5)
+                    {
+                        g.FillEllipse(new SolidBrush(Color.White), new Rectangle(bPoints[i].X, bPoints[i].Y, 5, 5));
+                        bPoints.RemoveAt(i);
+                        Draw();
+                    }
+            } else if (radioButton2.Checked)
+            {
+                CurrentPoint = e.Location;
+                for (int i = 0; i < bPoints.Count; i++)
+                    if (Math.Abs(CurrentPoint.X - bPoints[i].X) <= 5 && Math.Abs(CurrentPoint.Y - bPoints[i].Y) <= 5)
+                    {
+                        var dialogX = new Form2("Введите X");
+
+                        int X = 0;
+                        if (dialogX.ShowDialog() == DialogResult.Cancel) return;
+                        if (!int.TryParse(dialogX.ResultText, out X)) return;
+
+                        var dialogY = new Form2("Введите Y");
+
+                        int Y = 0;
+                        if (dialogY.ShowDialog() == DialogResult.Cancel) return;
+                        if (!int.TryParse(dialogY.ResultText, out Y)) return;
+
+                        g.FillEllipse(new SolidBrush(Color.White), new Rectangle(bPoints[i].X, bPoints[i].Y, 5, 5));
+                        Point p = new Point(bPoints[i].X + X, bPoints[i].Y + Y);
+                        bPoints[i] = p;
+                        g.FillEllipse(new SolidBrush(Color.Black), new Rectangle(bPoints[i].X, bPoints[i].Y, 5, 5));
+                        Draw();
+                    }
+            }
+        }
+
+        public void Draw() {
+            if (!isDraw)
+                isDraw = true;
+            else
+                g.DrawLines(new Pen(Color.White), result);
+            int j = 0;
+            float step = 0.01f;
+
+            result = new PointF[101];
+            for (float t = 0; t < 1; t += step)
+            {
+                float ytmp = 0;
+                float xtmp = 0;
+                for (int i = 0; i < bPoints.Count; i++)
+                {
+                    float b = polinom(i, bPoints.Count - 1, t); 
+                    xtmp += bPoints[i].X * b;
+                    ytmp += bPoints[i].Y * b;
+                }
+                result[j] = new PointF(xtmp, ytmp);
+                j++;
+
+            }
+            g.DrawLines(new Pen(Color.Black), result);
+            area.Invalidate();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
